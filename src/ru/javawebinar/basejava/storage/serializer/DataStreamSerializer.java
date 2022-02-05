c336c42fa7c2614d1e0ec656444d630c13281ddb
WhiteK0T <alekssavenkov34@gmail.com>
2022-02-05 04:47:25 +0400

    HW10

--- src/ru/javawebinar/basejava/storage/serializer/DataStreamSerializer.java
+++ src/ru/javawebinar/basejava/storage/serializer/DataStreamSerializer.java
@@ -1,9 +1,11 @@
 package ru.javawebinar.basejava.storage.serializer;
 
-import ru.javawebinar.basejava.model.ContactType;
-import ru.javawebinar.basejava.model.Resume;
+import ru.javawebinar.basejava.model.*;
 
 import java.io.*;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 public class DataStreamSerializer implements StreamSerializer {
@@ -19,8 +21,46 @@ public class DataStreamSerializer implements StreamSerializer {
                 dos.writeUTF(entry.getKey().name());
                 dos.writeUTF(entry.getValue());
             }
-            // TODO implements sections
+            Map<SectionType, Section> sections = r.getSections();
+            dos.writeInt(sections.size());
+            for (Map.Entry<SectionType, Section> entry : sections.entrySet()) {
+                SectionType type = entry.getKey();
+                Section section = entry.getValue();
+                dos.writeUTF(type.name());
+                switch (type) {
+                    case PERSONAL, OBJECTIVE -> dos.writeUTF(((TextSection) section).getContent());
+                    case ACHIEVEMENT, QUALIFICATIONS -> {
+                        List<String> items = ((ListSection) section).getItems();
+                        dos.writeInt(items.size());
+                        for (String item : items) {
+                            dos.writeUTF(item);
+                        }
+                    }
+                    case EXPERIENCE, EDUCATION -> {
+                        List<Organization> organizations = ((OrganizationSection) section).getOrganizations();
+                        dos.writeInt(organizations.size());
+                        for (Organization organization : organizations) {
+                            dos.writeUTF(organization.getHomePage().getName());
+                            dos.writeUTF(organization.getHomePage().getUrl());
+                            List<Organization.Position> positions = organization.getPositions();
+                            dos.writeInt(positions.size());
+                            for (Organization.Position position : positions) {
+                                writeLocalDate(dos, position.getStartDate());
+                                writeLocalDate(dos, position.getEndDate());
+                                dos.writeUTF(position.getTitle());
+                                dos.writeUTF(position.getDescription());
+                            }
+                        }
+                    }
+                }
+            }
         }
+    }
+
+    private void writeLocalDate(DataOutputStream dos, LocalDate localDate) throws IOException {
+        dos.writeInt(localDate.getYear());
+        dos.writeInt(localDate.getMonth().getValue());
+        dos.writeInt(localDate.getDayOfMonth());
     }
 
     @Override
@@ -33,7 +73,38 @@ public class DataStreamSerializer implements StreamSerializer {
             for (int i = 0; i < size; i++) {
                 resume.addContact(ContactType.valueOf(dis.readUTF()), dis.readUTF());
             }
-            // TODO implements sections
+            size = dis.readInt();
+            for (int i = 0; i < size; i++) {
+                SectionType type = SectionType.valueOf(dis.readUTF());
+                Section section = switch (type) {
+                    case PERSONAL, OBJECTIVE -> new TextSection(dis.readUTF());
+                    case ACHIEVEMENT, QUALIFICATIONS -> {
+                        int length = dis.readInt();
+                        ArrayList<String> list = new ArrayList<>();
+                        for (int j = 0; j < length; j++) {
+                            list.add(dis.readUTF());
+                        }
+                        yield new ListSection(list);
+                    }
+                    case EXPERIENCE, EDUCATION -> {
+                        ArrayList<Organization> organizations = new ArrayList<>();
+                        int lengthOrganizations = dis.readInt();
+                        for (int j = 0; j < lengthOrganizations; j++) {
+                            Link link = new Link(dis.readUTF(), dis.readUTF());
+                            ArrayList<Organization.Position> listPositions = new ArrayList<>();
+                            int lengthPosition = dis.readInt();
+                            for (int k = 0; k < lengthPosition; k++) {
+                                LocalDate startDate = LocalDate.of(dis.readInt(), dis.readInt(), dis.readInt());
+                                LocalDate endDate = LocalDate.of(dis.readInt(), dis.readInt(), dis.readInt());
+                                listPositions.add(new Organization.Position(startDate, endDate, dis.readUTF(), dis.readUTF()));
+                            }
+                            organizations.add(new Organization(link, listPositions));
+                        }
+                        yield new OrganizationSection(organizations);
+                    }
+                };
+                resume.addSection(type, section);
+            }
             return resume;
         }
     }
