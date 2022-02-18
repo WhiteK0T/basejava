5e08b0cfe7e1ae74725eb11033f1adf5f522115b
WhiteK0T <alekssavenkov34@gmail.com>
2022-02-18 08:39:58 +0400

    HW13

--- src/ru/javawebinar/basejava/Config.java
+++ src/ru/javawebinar/basejava/Config.java
@@ -1,5 +1,8 @@
 package ru.javawebinar.basejava;
 
+import ru.javawebinar.basejava.storage.SqlStorage;
+import ru.javawebinar.basejava.storage.Storage;
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -7,11 +10,12 @@ import java.io.InputStream;
 import java.util.Properties;
 
 public class Config {
-    private static final File PROPS = new File("config\\resumes.properties");
+
+    private static final File PROPS = new File("src/ru/javawebinar/basejava/config/resumes.properties");
     private static final Config INSTANCE = new Config();
 
-    private Properties props = new Properties();
-    private File storageDir;
+    private final File props;
+    private final Storage storage;
 
     public static Config get() {
         return INSTANCE;
@@ -19,14 +23,21 @@ public class Config {
 
     private Config() {
         try (InputStream is = new FileInputStream(PROPS)) {
+            Properties props = new Properties();
             props.load(is);
-            storageDir = new File(props.getProperty("storage.dir"));
+            this.props = new File(props.getProperty("storage.dir"));
+            this.storage = new SqlStorage(props.getProperty("db.url"), props.getProperty("db.user"),
+                    props.getProperty("db.password"));
         } catch (IOException e) {
             throw new IllegalStateException("Invalid config file " + PROPS.getAbsolutePath());
         }
     }
 
     public File getStorageDir() {
-        return storageDir;
+        return props;
+    }
+
+    public Storage getSQLStorage() {
+        return storage;
     }
 }
---
+++ src/ru/javawebinar/basejava/sql/SqlHelper.java
@@ -0,0 +1,33 @@
+package ru.javawebinar.basejava.sql;
+
+import org.postgresql.util.PSQLException;
+import ru.javawebinar.basejava.exception.ExistStorageException;
+import ru.javawebinar.basejava.exception.StorageException;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class SqlHelper {
+
+    private final ConnectionFactory connectionFactory;
+
+    public SqlHelper(ConnectionFactory connectionFactory) {
+        this.connectionFactory = connectionFactory;
+    }
+
+    public <T> T execute(String sql, SqlExecutor<T> executor) {
+        try (Connection conn = connectionFactory.getConnection();
+             PreparedStatement ps = conn.prepareStatement(sql)) {
+            return executor.execute(ps);
+        } catch (SQLException e) {
+            if (e instanceof PSQLException) {
+                //http://www.postgresql.org/docs/9.3/static/errcodes-appendix.html
+                if (e.getSQLState().equals("23505")) {
+                    throw new ExistStorageException(null);
+                }
+            }
+            throw new StorageException(e);
+        }
+    }
+}
