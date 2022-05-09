6d4095f44d64ab607601d493f51e3b6ff3f03335
WhiteK0T <alekssavenkov34@gmail.com>
2022-05-09 08:28:01 +0400

    DB credentials from Sys ENV "DATABASE_URL"

--- src/ru/javawebinar/basejava/Config.java
+++ src/ru/javawebinar/basejava/Config.java
@@ -7,9 +7,13 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
+import java.util.logging.Logger;
 
 public class Config {
+
+    private static final Logger LOG = Logger.getLogger(Config.class.getName());
     private static final String PROPS = "/resumes.properties";
+
     private static final Config INSTANCE = new Config();
 
     private final File storageDir;
@@ -24,7 +28,7 @@ public class Config {
             Properties props = new Properties();
             props.load(is);
             storageDir = new File(props.getProperty("storage.dir"));
-            storage = new SqlStorage(props.getProperty("db.url"), props.getProperty("db.user"), props.getProperty("db.password"));
+            storage = getCredentialsFromEnvironmentVariablesOrDefault(props);
         } catch (IOException e) {
             throw new IllegalStateException("Invalid config file " + PROPS);
         }
@@ -36,6 +40,17 @@ public class Config {
 
     public Storage getStorage() {
         return storage;
+    }
+
+    private Storage getCredentialsFromEnvironmentVariablesOrDefault(Properties props) {
+        String databaseUrl = System.getenv("DATABASE_URL");
+        if (databaseUrl == null) {
+            return new SqlStorage(props.getProperty("db.url"), props.getProperty("db.user"), props.getProperty("db.password"));
+        }
+        LOG.info("Find DATABASE_URL");
+        String[] split = databaseUrl.split("[:@/]");
+        String dbUrl = "jdbc:postgresql://" + databaseUrl.split("@")[1];
+        return new SqlStorage(dbUrl, split[3], split[4]);
     }
 
 }
